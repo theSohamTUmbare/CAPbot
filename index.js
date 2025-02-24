bc7e3b4d640fda4846c45569caed8b726f83dfc1
Soham <umbaresoham18@gmail.com>
2025-02-24 20:59:43 +0530

    Added small chat functionality next step next step is to add thee image captioning

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+});
+
+client.on("messageCreate", (message) => { 
+    if (message.author.bot) return; 
+    message.reply({ 
+        content: "Hi From Bot", 
+    });
+});
+
+client.login(
+    process.env.SECRET_TOKEN
+)
